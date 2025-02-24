7056303adb0c9384919ba99e8d8b77495e4536cf
Kanto <kanto@mg-consulting-it-act.com>
2025-02-24 15:47:13 +0300

    page la biosthetique

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
           Arrivée en Belgique en 2017, Letizia croise le chemin de Jenny dans un salon de Watermael-Boitsfort. Très vite, une alchimie naturelle s’installe entre elles, <br> tant sur le plan professionnel que personnel. <br> Ce qui n’était au départ qu’une belle rencontre est devenu une évidence : créer ensemble un espace où chaque client se sent unique et sublimé. </p>
         <p>C’est ainsi qu’est né "Stylé, de Bruxelles à Palermo". <br> Depuis le 1ᵉʳ juillet 2020, leur salon situé à Auderghem est un véritable écrin dédié à la      beauté capillaire, où le savoir-faire italien et la précision belge se conjuguent harmonieusement.Que vous souhaitiez une coupe tendance, une coloration éclatante ou un soin expert, Jenny et Letizia vous accueillent avec passion et bienveillance. <br> Parce que votre style est notre signature.</p>
         <div class="py-8">
-          <button class="border border-[#E09550] px-10 py-3 text-[#E09550] rounded-full mr-10 shadow-md">Contactez-nous</button>
+          <button class="border border-[#E09550] px-10 py-3 text-[#E09550] rounded-full mr-10 shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s">Contactez-nous</button>
           <button onclick="window.open('https://client.optios.net/online_booking.html?/7827', '_blank', 'noopener')" class="border px-10 py-3 bg-[#E09550] text-white rounded-full shadow-md hover:bg-white hover:text-[#E09550] transition ease-in-out duration-500">Rendez-vous en ligne</button>
         </div>
       </div>
@@ -50,7 +50,7 @@
       <p class="text-5xl text-white font-bold leading-normal">Contactez-nous <br> pour un rendez-vous !</p>
       <div class="flex flex-col gap-6">
         <button onclick="window.open('https://client.optios.net/online_booking.html?/7827', '_blank', 'noopener')" class="border px-10 py-3 bg-[#E09550] text-white rounded-full shadow-md hover:bg-white hover:text-[#E09550] transition ease-in-out duration-500">Rendez-vous en ligne</button>
-        <button class="border border-[#E09550] px-10 py-3 text-[#E09550] rounded-full bg-white shadow-md">Contactez-nous</button>
+        <button class="border border-[#E09550] px-10 py-3 text-[#E09550] rounded-full bg-white shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s">Contactez-nous</button>
       </div>
     </div>
   </section>
@@ -60,7 +60,7 @@
       <h2 class="text-[#E09550] text-4xl font-bold">Quand la Beauté Rencontre l’Écologie :</h2>
       <h3 class="text-2xl font-bold">Stylé x La Biosthétique</h3>
       <p>Chez Stylé, nous croyons en une coiffure qui allie élégance et engagement. De Bruxelles à Palerme, notre mission est de sublimer chaque chevelure tout en respectant la planète. C’est pourquoi nous sommes fiers d’annoncer notre collaboration avec La Biosthétique, une marque d’excellence qui partage nos valeurs de soin, d’innovation et de durabilité. Ensemble, nous donnons une seconde vie aux cheveux coupés grâce à un programme de recyclage capillaire révolutionnaire.</p>
-      <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md">En savoir plus</button>
+     <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s"> <NuxtLink to="/indexDetails/labiosthetique">En savoir plus</NuxtLink></button>
     </div>
     <div class="uppercase border-2 border-[#E09550] w-1/2 h-[600px] flex flex-col justify-center items-center text-center relative">
       <h1 class="text-6xl font-bold leading-[0.5] scale-y-150 relative">la biosthetique <br><span class="font-normal text-2xl">paris</span></h1>
@@ -78,7 +78,7 @@
       <p>Les cheveux récupérés sont transformés en solutions éco-responsables, contribuant à la dépollution et à la préservation de l’environnement.</p>
       <p class="text-[#E09550]">Un geste beauté, un impact positif.</p>
       <p>En choisissant Stylé et La Biosthétique, vous optez pour une coiffure d’exception tout en participant activement à une démarche écologique. Parce que chaque coupe peut faire la différence.</p>
-      <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md"><NuxtLink to="/indexDetails/hairRecycle">En savoir plus</NuxtLink></button>
+     <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s"><NuxtLink to="/indexDetails/hairRecycle">En savoir plus </NuxtLink></button>
     </div>
   </section>
 
@@ -111,7 +111,7 @@
         Parce que votre style est notre signature.
       </p>
       <div class="space-x-10">
-        <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md">Contactez-nous</button>
+        <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s">Contactez-nous</button>
         <button onclick="window.open('https://client.optios.net/online_booking.html?/7827', '_blank', 'noopener')" class="border px-10 py-3 bg-[#E09550] text-white rounded-full shadow-md hover:bg-white hover:text-[#E09550] transition ease-in-out duration-500">Rendez-vous en ligne</button>
       </div>
     </div>
---
+++ pages/indexDetails/labiosthetique.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="services">
+    <div class="relative">
+      <img src="/images/slide2.svg" alt="image2" class="w-full object-cover">
+    </div>
+    <div class="w-4/5 h-4 bg-[#E09550] rounded-r-lg"></div>
+
+    <div class="my-20 px-20 flex justify-between items-center">
+        <div class="uppercase border-2 border-[#E09550] w-1/2 h-[600px] flex flex-col justify-center items-center text-center relative">
+            <h1 class="text-6xl font-bold leading-[0.5] scale-y-150 relative">la biosthetique <br><span class="font-normal text-2xl">paris</span></h1>
+            <p class="absolute bottom-16 text-2xl">the culture of total beauty</p>
+        </div>
+        <div class="w-1/2 space-y-10 px-20">
+            <h2 class="text-[#E09550] text-4xl font-bold">Quand la Beauté Rencontre l’Écologie :</h2>
+            <h3 class="text-2xl font-bold">Stylé x La Biosthétique</h3>
+            <p>Chez Stylé, nous croyons en une coiffure qui allie élégance et engagement. De Bruxelles à Palerme, notre mission est de sublimer chaque chevelure tout en respectant la planète. C’est pourquoi nous sommes fiers d’annoncer notre collaboration avec La Biosthétique, une marque d’excellence qui partage nos valeurs de soin, d’innovation et de durabilité. Ensemble, nous donnons une seconde vie aux cheveux coupés grâce à un programme de recyclage capillaire révolutionnaire.</p>
+            <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s"> <NuxtLink to="/indexDetails/labiosthetique">En savoir plus</NuxtLink></button>
+        </div>
+    </div>
+
+    <div class="w-full">
+        <img src="/images/img_bleu.svg" alt="image-bleu" class="w-full">
+    </div>
+
+    <div class="my-10 px-20 flex justify-between items-center mb-40">
+        <div class="w-1/2 space-y-10 px-20">
+            <h2 class="text-[#E09550] text-4xl font-bold">Stylé, de Bruxelles à Palermo</h2>
+            <h3 class="text-2xl font-bold">L’art de la coiffure sans frontières</h3>
+            <p>2344 kilomètres et 11 ans d’expériences les séparaient, mais leur passion les a réunies. <br>
+                Jenny, bruxelloise, et Letizia, originaire de Palerme, <br>
+                partagent une vision commune de la coiffure : élégance, savoir-faire et authenticité. <br>
+                Arrivée en Belgique en 2017, Letizia croise le chemin de Jenny dans un salon de Watermael-Boitsfort. Très vite, une alchimie naturelle s’installe entre elles, <br>
+                tant sur le plan professionnel que personnel. <br>
+                Ce qui n’était au départ qu’une belle rencontre est devenu une évidence : créer ensemble un espace où chaque client se sent unique et sublimé. 
+                </p>
+            <p>C’est ainsi qu’est né "Stylé, de Bruxelles à Palermo". <br>
+                Depuis le 1ᵉʳ juillet 2020, leur salon situé à Auderghem est un véritable écrin dédié à la beauté capillaire, où le savoir-faire italien et la précision belge se conjuguent harmonieusement. <br>
+                Que vous souhaitiez une coupe tendance, une coloration éclatante ou un soin expert, <br>
+                Jenny et Letizia vous accueillent avec passion et bienveillance. <br>
+                Parce que votre style est notre signature.
+            </p>
+            <div class="space-x-10">
+                <button class="border border-[#E09550] px-20 py-3 text-[#E09550] rounded-full bg-white shadow-md hover:bg-[#E09550] hover:text-white transition easy 5s">Contactez-nous</button>
+                <button onclick="window.open('https://client.optios.net/online_booking.html?/7827', '_blank', 'noopener')" class="border px-10 py-3 bg-[#E09550] text-white rounded-full shadow-md hover:bg-white hover:text-[#E09550] transition ease-in-out duration-500">Rendez-vous en ligne</button>
+            </div>
+        </div>
+        <div class="w-1/2">
+            <img src="/images/img_biosthetique.svg" alt="img_coiffure" class="w-full" style="border-top-left-radius:200px">
+        </div>
+    </div>
+
+  </section>
+</template>
+
+<script setup lang="ts">
+// Code spécifique à la page
+</script>
+
+<style scoped>
+
+</style>
+  
