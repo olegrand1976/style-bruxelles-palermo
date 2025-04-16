e691800f24c9870b45d053106da81a341ce66fc7
Kanto <kanto@mg-consulting-it-act.com>
2025-04-16 14:43:29 +0300

    mobile navbar

--- components/Header.vue
+++ components/Header.vue
@@ -24,15 +24,15 @@
     </nav>
 
     <!--mobile navbar-->
-    <div v-if="isMenuOpen" class="xl:hidden bg-white w-9/12 h-auto transition-all duration-300 overflow-hidden z-20 absolute right-0 shadow-xl">
+    <div v-if="isMenuOpen" class="xl:hidden bg-white w-5/6 h-auto transition-all duration-300 overflow-hidden z-20 absolute right-0 shadow-xl">
       <div class="px-8">
-        <div @click="toggleMenu" class="pt-8">
+        <div @click="toggleMenu" class="pt-2">
           <img src="/images/close.svg" alt="close">
         </div>
-        <div class="flex justify-end px-10">
-          <img src="/images/logo-nav.svg" alt="Stylé" class="w-32">
+        <div class="flex justify-end">
+          <img src="/images/logo-nav.svg" alt="Stylé" class="w-24">
         </div>
-        <nav class="text-xl">
+        <nav class="text-base">
           <NuxtLink :to="localePath('/')" class="block py-4 font-bold">{{ $t('accueil.title') }}</NuxtLink>
           <NuxtLink :to="localePath('/services')" class="block py-4">{{ $t('services.title')}}</NuxtLink>
           <NuxtLink :to="localePath('/contact')" class="block py-4">{{ $t('contact.title')}}</NuxtLink>
@@ -46,7 +46,7 @@
           <h2 class="font-bold">{{ $t('footer.text1') }} <br> Stylé de Bruxelles a Palermo</h2>
           <p>Avenue Jean Van Horenbeeck 1 <br> 1160 Bruxelles</p>
           <p class="font-bold">{{ $t('footer.text2') }} : <span class="font-normal">02/ 660 07 08</span></p>
-          <p class="font-bold">{{ $t('footer.text3') }}  : <a href="#" class="font-normal text-blue-500">style.bxl.plm@gmail.com</a></p>
+          <p class="font-bold">{{ $t('footer.text3') }}  : <a href="style.bxl.plm@gmail.com" target="_blank" class="font-normal text-blue-500">style.bxl.plm@gmail.com</a></p>
           <p class="font-bold">{{ $t('footer.text4') }}  <br> <span class="font-normal">{{ $t('footer.text5') }} </span></p>
           <p><span class="font-bold">{{ $t('footer.text8') }}</span> {{ $t('footer.text9') }} </p>
         </div>
@@ -54,10 +54,10 @@
           <label class="font-bold">newsletter</label>
           <p>Inscrivez-vous à notre newsletter pour recevoir nos futures communications.</p>
           <div class="border border-gray-300 rounded-lg mt-2 flex justify-between">
-            <input type="email" placeholder="Enter Your Email" class="p-4 rounded-lg w-full outline-none">
-            <button type="button" class="border border-[#E09550] py-4 px-8 rounded-lg font-bold text-white bg-[#E09550]" style="box-shadow:inset 0 2px 4px 0px rgba(0,0,0,0.3)">Subscribe</button>
+            <input type="email" placeholder="Enter Your Email" class="p-2 rounded-lg w-full outline-none">
+            <button type="button" class="border border-[#E09550] py-2 px-8 rounded-lg font-bold text-white bg-[#E09550]" style="box-shadow:inset 0 2px 4px 0px rgba(0,0,0,0.3)">Subscribe</button>
           </div>
-          <div class="flex items-center py-4 gap-4 text-4xl text-[#E09550]">
+          <div class="flex items-center py-4 gap-4 text-2xl text-[#E09550]">
             <Icon name="uil:twitter" class="cursor-pointer" />
             <Icon name="uil:facebook" class="cursor-pointer" />
             <Icon name="uil:instagram" class="cursor-pointer" />
